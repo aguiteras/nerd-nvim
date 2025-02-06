3ac178637aff532537d8282f9b2cb9d4fad92c0a
Arnau Guiteras <arnau.guiteras@weareadaptive.com>
2025-02-06 15:25:53 +0100

    modified git-stuff, added nvim-tmux and modified lsp-config

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -5,7 +5,7 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gitsigns", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
 
     end
   },
